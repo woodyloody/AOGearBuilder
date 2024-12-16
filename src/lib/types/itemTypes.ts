0199337fd9a4fd47cd74a266264cb316395c5bbb
BobbyNooby <theengineering.commandgamer@gmail.com>
2024-12-16 00:14:30 +0000

    Item Types so far

--- src/lib/types/itemTypes.ts
+++ src/lib/types/itemTypes.ts
@@ -81,3 +81,61 @@ export type GearEnchantStats = Partial<{
 	warding: number;
 	drawback: number;
 }>;
+
+type statsPerLevel = {
+	level: number;
+} & GearStats;
+
+export type ArmorDetails = ItemIdentifiers & {
+	mainType: 'Accessory' | 'Chestplate' | 'Pants';
+	gemNo: number;
+	minLevel: number;
+	maxLevel: number;
+	statsPerLevel: statsPerLevel[];
+	validModifiers: { id: string; name: string }[];
+};
+
+export type GemDetails = ItemIdentifiers & GearStats;
+
+export type ShipPartDetails = ItemIdentifiers & {
+	mainType:
+		| 'Cannon'
+		| 'Hull Armor'
+		| 'Deckhand'
+		| 'Ram'
+		| 'Sail Material'
+		| 'Siege Weapon'
+		| 'Boat'
+		| 'Ship Crew'
+		| 'Quartermaster'
+		| 'Ship';
+} & ShipStats;
+
+export type EnchantDetails = ItemIdentifiers & {
+	mainType: 'Enchant';
+	enchantTypes: Partial<{
+		gear: GearEnchantStats;
+		ship: Record<'ram' | 'hull' | 'sail', ShipStats>;
+	}>;
+};
+
+export type ModifierDetails = ItemIdentifiers &
+	GearEnchantStats & {
+		mainType: 'Modifier';
+	};
+
+export type AnyItemDetails = Partial<
+	ArmorDetails | GemDetails | ShipPartDetails | EnchantDetails | ModifierDetails
+>;
+
+export type ShipDetails = ItemIdentifiers &
+	ShipStats & {
+		hullArmorSlot: number;
+		quartermasterSlot: number;
+		cannonSlot: number;
+		siegeWeaponSlot: number;
+		sailMaterialSlot: number;
+		shipCrewSlot: number;
+		ramSlot: number;
+		deckhandSlot: number;
+	};
